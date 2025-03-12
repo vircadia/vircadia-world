9ca95289686a3a7b79946299483d079c467bccae
Kalila <69767640+digisomni@users.noreply.github.com>
2025-03-12 20:50:51 +0800

    Update env / configs to work in vite/node/bun.

--- client/web_babylon_js/volume/app/vite.config.ts
+++ client/web_babylon_js/volume/app/vite.config.ts
@@ -1,21 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { VircadiaConfig } from "../vircadia-world-sdk-ts/config/vircadia.config";
 
 export default defineConfig({
     plugins: [solidPlugin(), tailwindcss()],
-    // server: {
-    //     port: Number.parseInt(
-    //         import.meta.env
-    //             .VRCA_CLIENT_WEB_BABYLON_JS_DEV_PORT_CONTAINER_INTERNAL as string,
-    //     ),
-    // },
-    // preview: {
-    //     port: Number.parseInt(
-    //         import.meta.env
-    //             .VRCA_CLIENT_WEB_BABYLON_JS_PRODUCTION_PORT_CONTAINER_INTERNAL as string,
-    //     ),
-    // },
+    server: {
+        port: VircadiaConfig.CLIENT
+            .VRCA_CLIENT_WEB_BABYLON_JS_DEV_PORT_CONTAINER_INTERNAL,
+        strictPort: true,
+    },
+    preview: {
+        port: VircadiaConfig.CLIENT
+            .VRCA_CLIENT_WEB_BABYLON_JS_PRODUCTION_PORT_CONTAINER_INTERNAL,
+        strictPort: true,
+    },
     envPrefix: "VRCA_CLIENT_",
     build: {
         target: "esnext",

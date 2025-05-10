fc15f1f3c5f79292977d4b8d65b2b5ef207f00cd
Kalila <69767640+digisomni@users.noreply.github.com>
2025-05-10 13:56:51 +0800

    Add new seeds.

---
+++ cli/database/seed/sql/babylon_levelTest.sql
@@ -0,0 +1,8 @@
+INSERT INTO entity.entity_assets (
+    general__asset_file_name,
+    group__sync,
+    asset__mime_type
+)
+VALUES
+    -- Babylon.js Level Test
+    ('babylon_levelTest.glb', 'public.STATIC', 'model/gltf-binary');
